d3db1fc4ff8124e269a2c471623f922967b2e4bd
tchclaravel <tchc100@gmail.com>
2024-11-27 10:58:56 +0300

    Start woking on order confirmation

--- Bulky.Models/OrderDetails.cs
+++ Bulky.Models/OrderDetails.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +11,21 @@ namespace Bulky.Models
 {
     public class OrderDetails
     {
+        public int Id { get; set; }
+        [Required]
+        public int OrderHeaderId { get; set; }
+        [ForeignKey("OrderHeaderId")]
+        [ValidateNever]
+        public OrderHeader OrderHeader { get; set; }
+
+
+        [Required]
+        public int ProductId { get; set; }
+        [ForeignKey("ProductId")]
+        [ValidateNever]
+        public Product Product { get; set; }
+
+        public int Count { get; set; }
+        public double Price { get; set; }
     }
 }
--- Bulky.Models/OrderHeader.cs
+++ Bulky.Models/OrderHeader.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
@@ -25,6 +26,20 @@ namespace Bulky.Models
 
         public DateTime PaymentDate { get; set; }
         public DateOnly PaymentDueDate { get; set; }
+        public string? PaymentIntentId { get; set; }
+
+        [Required]
+        public string PhoneNumber { get; set; }
+        [Required]
+        public string StreetAddress { get; set; }
+        [Required]
+        public string City { get; set; }
+        [Required]
+        public string State { get; set; }
+        [Required]
+        public string PostalCode { get; set; }
+        [Required]
+        public string Name { get; set; }
 
     }
 }
